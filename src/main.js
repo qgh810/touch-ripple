1db506c43f21ae2500057a87883706ce32c1bca8
邱国辉 <530784837@qq.com>
2017-04-03 23:30:55 +0800

    初始化项目

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { checkNode } from './utils/check'
+import { showWarn } from './utils/log'
+
+class ClickResponse {
+  constructor (el, options) {
+
+  }
+}
+
+module.exports = ClickResponse
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var webpack = require('webpack')
+var libraryName = 'ClickResponse'
+
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    path: __dirname,
+    filename: "dist/click-response.js",
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  plugins: [
+    // new webpack.optimize.UglifyJsPlugin({
+    //   output: {
+    //     comments: false,  // remove all comments
+    //   },
+    //   compress: {
+    //     warnings: false
+    //   }
+    // })
+  ]
+}
